1e77d9422df3f84173d5a69bb06f6fc36668ed7b
sivertigo <sg.tak22@gmail.com>
2017-05-02 13:46:50 +0900

    change the root dir

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 echo "start deploying the weblog."
 hugo
-rm -rf ./sivertigo.github.io/docs
-cp -rf ./docs ./sivertigo.github.io/docs
-cp -rf ./content ./sivertigo.github.io/docs/content
+rm -rf ./sivertigo.github.io/*
+cp ./CNAME ./sivertigo.github.io/
+
+cp -rf ./docs/* ./sivertigo.github.io/
+cp -rf ./content ./sivertigo.github.io/content
 cd ./sivertigo.github.io
 git add .
 git commit -m "add new articles"
